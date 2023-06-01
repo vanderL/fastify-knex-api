39a79505fd0293378f1d0876bf11276a04e411f1
Vander Lima <vnd.vander@gmail.com>
2023-05-31 22:58:11 -0300

    init

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/hello', () => {
+  return 'hello world'
+})
+
+app.listen({
+  port: 3002
+}).then(() => {
+  console.log("Http server running")
+})
